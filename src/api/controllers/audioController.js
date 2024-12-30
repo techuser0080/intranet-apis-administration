b1b48c91cc0a798fc7c16b20b0bd7915f1d60aeb
Rodrigo Burgos <techuser080@gmail.com>
2024-12-29 19:29:21 -0500

    feat(add constants and response entity)

--- src/api/controllers/audioController.js
+++ src/api/controllers/audioController.js
@@ -1,9 +1,15 @@
+import { Constants } from "../../config/constants.js"
 import { uploadAudioToFolderService } from "../services/audioService.js"
 
 export const uploadAudioToFolder = async(req, res) => {
-    const { folderId } = req.params
-    const { creationUserId } = req.body
-    const { result } = await uploadAudioToFolderService(folderId, creationUserId)
-    if(result == 2) res.sendStatus(500)
-    res.sendStatus(200)
+    try {
+        const { folderId } = req.params
+        const { creationUserId } = req.body
+        const { result } = await uploadAudioToFolderService(folderId, creationUserId)
+        if (result == null) res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+        if (result == 2) res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+        res.sendStatus(200).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+    } catch (error) {
+        res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+    }
 }
--- src/api/controllers/folderController.js
+++ src/api/controllers/folderController.js
@@ -1,15 +1,24 @@
+import { Constants } from "../../config/constants.js"
 import { getFoldersByCompanyIdService, createFolderService } from "../services/folderService.js"
 
 export const getFoldersByCompanyId = async(req, res) => {
-    const { companyId } = req.params
-    const results = await getFoldersByCompanyIdService(companyId)
-    res.send(results)
+    try {
+        const { companyId } = req.params
+        const results = await getFoldersByCompanyIdService(companyId)
+        res.sendStatus(200).json(1, Constants.MESSAGE_STATUS_OK, results)
+    } catch (error) {
+        res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+    }
 }
 
 export const createFolder = async(req, res) => {
-    const { description, companyId, creationUserId } = req.body
-    const { result } = await createFolderService(description, companyId, creationUserId)
-    console.log(result)
-    if (result == 2) res.sendStatus(500)
-    res.sendStatus(200)
+    try {
+        const { description, companyId, creationUserId } = req.body
+        const { result } = await createFolderService(description, companyId, creationUserId)
+        if (result == null) res.sendStatus(2, Constants.MESSAGE_STATUS_ERROR, null)
+        if (result == 2) res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+        res.sendStatus(200).json(1, Constants.MESSAGE_STATUS_OK, null)
+    } catch (error) {
+        res.sendStatus(500).json(2, Constants.MESSAGE_STATUS_ERROR, null)
+    }
 }
