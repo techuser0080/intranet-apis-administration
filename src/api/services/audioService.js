403163e55278f2c145daba10538dc4be172b4e37
Rodrigo Burgos <techuser080@gmail.com>
2025-03-26 14:12:35 -0500

    fix(remove log from error catch upload audio)

--- src/api/services/audioService.js
+++ src/api/services/audioService.js
@@ -4,7 +4,6 @@ export const uploadAudioToFolderService = async(folderId, transcript, creationUs
     const result = await new Promise((resolve, reject) => {
         pool.query('CALL spUploadAudioToFolder(?,?,?,@statusCode); SELECT @statusCode AS result;', [folderId, transcript, 
             creationUserId], (err, results) => {
-                console.log(err)
                 if (err) reject(new Error(err.message))
                 resolve(results[1][0])
         })
