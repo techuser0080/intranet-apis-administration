10d9531b94e06f98fab8c31460ab00991ee06691
Rodrigo Burgos <techuser080@gmail.com>
2025-03-29 11:16:40 -0500

    configuration constants and database

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2'
 import { Constants } from './constants.js'
 
 export const pool = mysql.createPool({
-    host: Constants.SECURITY_DATABASE_HOST,
-    user: Constants.SECURITY_DATABASE_USER_NAME,
-    port: Constants.SECURITY_DATABASE_PORT,
-    password: Constants.SECURITY_DATABASE_PASSWORD,
-    database: Constants.SECURITY_DATABASE_NAME,
+    host: Constants.ADMINISTRATION_DATABASE_HOST,
+    user: Constants.ADMINISTRATION_DATABASE_USER_NAME,
+    port: Constants.ADMINISTRATION_DATABASE_PORT,
+    password: Constants.ADMINISTRATION_DATABASE_PASSWORD,
+    database: Constants.ADMINISTRATION_DATABASE_NAME,
     multipleStatements: true,
     ssl: {
         rejectUnauthorized: true
